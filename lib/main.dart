7f68305c75c0f24e98c878330c823ae2ded66a81
Aslısu Sapmaz <72272463+Aslisusapmaz@users.noreply.github.com>
2021-02-06 01:59:23 +0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/signup_screen.dart';
-import 'package:provider/provider.dart';
-import 'models/authentication.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider.value(value: Authentication(),
-        )
-      ],
-      child:MaterialApp(
+    return MaterialApp(
       title: 'ToDo App',
       theme: ThemeData(
         primaryColor: Colors.lightBlueAccent,
       ),
       home: LoginScreen(),
-       routes: {
-         SignupScreen.routeName:(context)=>SignupScreen(),
-         LoginScreen.routeName:(context)=>LoginScreen(),
-         HomeScreen.routeName:(context)=>HomeScreen(),
-       },
-      )
+      routes: {
+        SignupScreen.routeName: (context) => SignupScreen(),
+        LoginScreen.routeName: (context) => LoginScreen(),
+        HomeScreen.routeName: (context) => HomeScreen(),
+      },
     );
   }
 }
-
-
