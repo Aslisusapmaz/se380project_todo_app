a379323d079c752504baa5d10edf6ef1eb882741
Aslısu Sapmaz <72272463+Aslisusapmaz@users.noreply.github.com>
2021-02-06 01:58:53 +0300

    second commit

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,54 +1,56 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'signup_screen.dart';
-import 'package:provider/provider.dart';
-import '../models/authentication.dart';
 import 'home_screen.dart';
+
 class LoginScreen extends StatefulWidget {
-  static const routeName='/login';
+  static const routeName = '/login';
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey();
-  Map<String ,String> _authData={
-'email':'',
-    'password':''
-
-  };
-  void _showErrorDialog(String message){
-    showDialog(context:context,
-      builder: (context)=>AlertDialog(
-        title: Text('An Error Occured'),
-        content:Text(message),
-        actions:<Widget> [
-          FlatButton(
-              child: Text('Okay'),
-            onPressed: (){
-                Navigator.of(context).pop();
-            },
-          )
-        ],
-      )
-    );
+  Map<String, String> _authData = {'email': '', 'password': ''};
+  void _showErrorDialog(String message) {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: Text('An Error Occured'),
+              content: Text(message),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('Okay'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ));
   }
 
-  Future<void> _done() async{
-    if (!_formKey.currentState.validate())
-    {
+  Future<void> _done() async {
+    if (!_formKey.currentState.validate()) {
       return;
     }
     _formKey.currentState.save();
-    try{
-      await Provider.of<Authentication>(context,listen:false).login(_authData['email'], _authData['password']);
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: _authData["email"], password: _authData["password"]);
       Navigator.of(context).pushReplacementNamed(HomeScreen.routeName);
-    }catch(error)
-    {
-        var errorMessage='Authentication Failed. Please try again later.';
-        _showErrorDialog(errorMessage);
+    } catch (error) {
+      var errorMessage = 'Authentication Failed. Please try again later.';
+      _showErrorDialog(errorMessage);
     }
+  }
 
-}
+  @override
+  void initState() {
+    Firebase.initializeApp();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,16 +59,12 @@ class _LoginScreenState extends State<LoginScreen> {
         actions: <Widget>[
           FlatButton(
             child: Row(
-              children:<Widget> [
-                Text('Signup'),
-                Icon(Icons.person_add)
-
-              ],
+              children: <Widget>[Text('Signup'), Icon(Icons.person_add)],
             ),
             textColor: Colors.white,
             onPressed: () {
-              Navigator.of(context).pushReplacementNamed(SignupScreen.routeName);
-
+              Navigator.of(context)
+                  .pushReplacementNamed(SignupScreen.routeName);
             },
           )
         ],
@@ -75,13 +73,10 @@ class _LoginScreenState extends State<LoginScreen> {
         children: <Widget>[
           Container(
             decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Colors.redAccent,
-                  Colors.blue,
-                ]
-              )
-            ),
+                gradient: LinearGradient(colors: [
+              Colors.redAccent,
+              Colors.blue,
+            ])),
           ),
           Center(
             child: Card(
@@ -100,52 +95,43 @@ class _LoginScreenState extends State<LoginScreen> {
                         TextFormField(
                           decoration: InputDecoration(labelText: 'Email'),
                           keyboardType: TextInputType.emailAddress,
-                          validator: (value)
-                             {
-                             if(value.isEmpty || !value.contains('@'))
-                            {
-                            return 'invalid email';
-                          }
-                          return null;
-                               },
-                          onSaved: (value)
-                              {
-                              _authData['email']=value;
+                          validator: (value) {
+                            if (value.isEmpty || !value.contains('@')) {
+                              return 'invalid email';
+                            }
+                            return null;
                           },
-
+                          onSaved: (value) {
+                            _authData['email'] = value;
+                          },
                         ),
                         TextFormField(
-                          decoration: InputDecoration(
-                            labelText: 'Password'),
-                            obscureText:true,
-                            validator:(value)
-                              {
-                                if(value.isEmpty || value.length<=5)
-                                  {
-                                    return 'invalid password';
-                                  }
-                                return null;
-
-                                },
-                                     onSaved:(value)
-                            {
-                       _authData['password']=value;
-                            }
-                          ),
-                        SizedBox(height: 30,),
+                            decoration: InputDecoration(labelText: 'Password'),
+                            obscureText: true,
+                            validator: (value) {
+                              if (value.isEmpty || value.length <= 5) {
+                                return 'invalid password';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _authData['password'] = value;
+                            }),
+                        SizedBox(
+                          height: 30,
+                        ),
                         RaisedButton(
-                            child:Text('Done'),
-                        onPressed: (){
-                                 _done();
-                        },
+                          child: Text('Done'),
+                          onPressed: () {
+                            _done();
+                          },
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(30),
                           ),
                           color: Colors.black,
                           textColor: Colors.white,
                         )
-                        ],
-
+                      ],
                     ),
                   ),
                 ),
