26de6bd904119cdfa58e522618e12d06b4f52d61
Aslısu Sapmaz <72272463+Aslisusapmaz@users.noreply.github.com>
2021-02-06 01:56:59 +0300

    second commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,20 +1,70 @@
 import 'package:flutter/material.dart';
-class HomeScreen extends StatelessWidget {
-  static const routeName='/home';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class HomeScreen extends StatefulWidget {
+  static const routeName = '/home';
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List todos = List();
+
+  String input = '';
+
+  createTodos() {
+    DocumentReference documentReference =
+        FirebaseFirestore.instance.collection('My Todos').doc(input);
+    Map<String, String> todos = {'todoTitle': input};
+    documentReference.set(todos).whenComplete(() {
+      print('$input created');
+    });
+  }
+
+  deleteTodos() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Screen'),
+        title: Text('My todos'),
       ),
-      body: Center(
-        child: Text(
-          'My home screen',style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 20,
-        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  title: Text('Add TodoList'),
+                  content: TextField(
+                    onChanged: (String value) {
+                      input = value;
+                    },
+                  ),
+                  actions: <Widget>[
+                    FlatButton(
+                        onPressed: () {
+                          createTodos();
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('Add'))
+                  ],
+                );
+              });
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
         ),
       ),
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('My Todos').snapshots(),
+          builder: (context, snapshots) {
+            return Container();
+          }),
     );
   }
 }
